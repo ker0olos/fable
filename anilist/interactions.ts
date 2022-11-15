e6b8c4bc90bfd15d033beb0fd5df0a898431a038
Kerolos Zaki <ker0olos@pm.me>
2022-11-15 09:41:25 +0200

    update std

--- anilist/interactions.ts
+++ anilist/interactions.ts
@@ -1,83 +1,57 @@
-import {
-  json,
-  serve,
-  validateRequest,
-} from 'https://deno.land/x/sift@0.5.0/mod.ts';
+import { serve } from 'https://deno.land/std@0.130.0/http/server.ts';
 
-import nacl from 'https://cdn.skypack.dev/tweetnacl@v1.0.3?dts';
+import { json, validateRequest, verifySignature } from '../index.ts';
 
-serve({
-  '/': async (request: Request) => {
-    const { error } = await validateRequest(request, {
-      POST: {
-        headers: ['X-Signature-Ed25519', 'X-Signature-Timestamp'],
+async function handler(request: Request): Promise<Response> {
+  const { error } = await validateRequest(request, {
+    POST: {
+      headers: ['X-Signature-Ed25519', 'X-Signature-Timestamp'],
+    },
+  });
+
+  if (error) {
+    return json({ error: error.message }, { status: error.status });
+  }
+
+  const { valid, body } = await verifySignature(
+    request,
+    Deno.env.get('DISCORD_PUBLIC_KEY')!,
+  );
+
+  if (!valid) {
+    return json(
+      { error: 'Invalid request' },
+      {
+        status: 401,
       },
-    });
-
-    if (error) {
-      return json({ error: error.message }, { status: error.status });
-    }
-
-    const { valid, body } = await verifySignature(request);
-
-    if (!valid) {
-      return json(
-        { error: 'Invalid request' },
-        {
-          status: 401,
-        },
-      );
-    }
-
-    const {
-      type = 0,
-      token = '',
-      data = { options: [] },
-      member = { user: { id: '' } },
-    } = JSON.parse(body);
-
-    if (type === 1) {
-      return json({
-        type: 1,
-      });
-    }
-
-    console.log(type, data, token, member);
-
-    if (type === 2 && data.name === 'ping') {
-      return json({
-        type: 4,
-        data: {
-          content: `pong`,
-        },
-      });
-    }
-
-    return json({ error: 'bad request' }, { status: 400 });
-  },
-});
-
-async function verifySignature(
-  request: Request,
-): Promise<{ valid: boolean; body: string }> {
-  const PUBLIC_KEY = Deno.env.get('DISCORD_PUBLIC_KEY')!;
-
-  const signature = request.headers.get('X-Signature-Ed25519')!;
-  const timestamp = request.headers.get('X-Signature-Timestamp')!;
-
-  const body = await request.text();
-
-  function hexToUint8Array(hex: string) {
-    return new Uint8Array(
-      hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)),
     );
   }
 
-  const valid = nacl.sign.detached.verify(
-    new TextEncoder().encode(timestamp + body),
-    hexToUint8Array(signature),
-    hexToUint8Array(PUBLIC_KEY),
-  );
+  const {
+    type = 0,
+    token = '',
+    data = { options: [] },
+    member = { user: { id: '' } },
+  } = JSON.parse(body);
 
-  return { valid, body };
+  if (type === 1) {
+    return json({
+      type: 1,
+    });
+  }
+
+  console.log(type, data, token, member);
+
+  if (type === 2 && data.name === 'ping') {
+    return json({
+      type: 4,
+      data: {
+        content: `pong`,
+      },
+    });
+  }
+
+  return json({ error: 'bad request' }, { status: 400 });
 }
+
+serve(handler);
