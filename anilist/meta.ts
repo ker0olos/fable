28c1f1a74021b54d3d5a0bcfbcd49bb99a8e290a
Kerolos Zaki <ker0olos@pm.me>
2022-11-20 18:29:11 +0200

    anilist: fix embed colors

--- anilist/meta.ts
+++ anilist/meta.ts
@@ -25,9 +25,9 @@ export function hexToInt(hex?: string): number | null {
 
   const color = hex.substring(1);
 
-  const R = color.substring(2, 2);
-  const G = color.substring(4, 2);
-  const B = color.substring(6, 2);
+  const R = color.substring(0, 2);
+  const G = color.substring(2, 4);
+  const B = color.substring(4, 6);
 
   return parseInt(`${R}${G}${B}`, 16);
 }
