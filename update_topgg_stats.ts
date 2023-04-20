dcb33874ce396614397b8fa956855fcb97393e76
Kerolos Zaki <ker0olos@pm.me>
2023-04-20 05:26:29 +0200

    fix topgg script `app_id` is undefined

--- update_topgg_stats.ts
+++ update_topgg_stats.ts
@@ -7,13 +7,16 @@ import {
   type query as _query,
 } from 'https://deno.land/x/fauna@5.0.0-deno-alpha9/mod.d.ts';
 
-import config from './src/config.ts';
-
 const fql = query as typeof _query;
 
 if (import.meta.main) {
+  const APP_ID = Deno.env.get('APP_ID');
   const TOPGG_TOKEN = Deno.env.get('TOPGG_TOKEN');
   const FAUNA_SECRET = Deno.env.get('FAUNA_SECRET');
+
+  if (!APP_ID) {
+    throw new Error('APP_ID is not defined');
+  }
 
   if (!TOPGG_TOKEN) {
     throw new Error('TOPGG_TOKEN is not defined');
@@ -29,11 +32,11 @@ if (import.meta.main) {
     fql.Count(fql.Documents(fql.Collection('guild'))),
   );
 
-  console.log(`APP ID: ${config.appId}`);
+  console.log(`APP ID: ${APP_ID}`);
   console.log(`Server Count: ${serverCount}`);
 
   const response = await fetch(
-    `https://top.gg/api/bots/${config.appId}/stats`,
+    `https://top.gg/api/bots/${APP_ID}/stats`,
     {
       method: 'POST',
       headers: {
