93001e15313af1570c3390656f5264db1ff7d167
Kerolos Zaki <ker0olos@pm.me>
2023-06-23 08:53:05 +0300

    Hotfix (#191)
    * fix `NaNundefined` error in the gallery

--- src/utils.ts
+++ src/utils.ts
@@ -126,6 +126,10 @@ function capitalize(s: string | undefined): string | undefined {
 }
 
 function compact(n: number): string {
+  if (n <= 0) {
+    return '0';
+  }
+
   const units = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
   const index = Math.floor(Math.log10(Math.abs(n)) / 3);
   const value = n / Math.pow(10, index * 3);
