2c49604defd56bfc90094e431f5b4bb20d567218
Kerolos Zaki <ker0olos@pm.me>
2024-02-02 19:52:42 +0200

    add `/api/pack/:packId`

--- src/interactions.ts
+++ src/interactions.ts
@@ -1406,6 +1406,7 @@ if (import.meta.main) {
     '/api/user': communityAPI.user,
     '/api/publish': communityAPI.publish,
     '/api/popular': communityAPI.popular,
+    '/api/pack/:packId+': communityAPI.pack,
     '/webhooks/topgg': webhooks.topgg,
     '/external/*': utils.handleProxy,
     '/assets/:filename+': utils.serveStatic('../assets/public', {
